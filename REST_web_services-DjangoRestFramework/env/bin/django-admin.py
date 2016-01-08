55bc33a6aeca1909a82c97859aed3f9861584102
Evan Story <estory1@gmail.com>
2016-01-07 23:38:22 -0600

    * Initial commit. * Everything localized by virtualenv. * Will start a web server on port 8000, authenticate an admin user, and GET users from the Sqlite DB.

---
+++ REST_web_services-DjangoRestFramework/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/estory/Documents/syncable/home/Pro/C/SD/solutions-foundations/REST_web_services/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
